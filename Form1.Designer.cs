25e794d43a40cbdbdf5ae8846e49ad57bfc1d9cf
Roman Tarasiuk <roman.tarasiuk.l@gmail.com>
2017-05-05 09:29:20 +0300

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace BigLogSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelectLogFile = new System.Windows.Forms.Button();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnResultsToText = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultFormula = new System.Windows.Forms.TextBox();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // txtLogPath
+            // 
+            this.txtLogPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLogPath.Location = new System.Drawing.Point(86, 12);
+            this.txtLogPath.Name = "txtLogPath";
+            this.txtLogPath.Size = new System.Drawing.Size(386, 20);
+            this.txtLogPath.TabIndex = 0;
+            this.txtLogPath.TextChanged += new System.EventHandler(this.txtLogPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Log file path:";
+            // 
+            // btnSelectLogFile
+            // 
+            this.btnSelectLogFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectLogFile.Location = new System.Drawing.Point(478, 10);
+            this.btnSelectLogFile.Name = "btnSelectLogFile";
+            this.btnSelectLogFile.Size = new System.Drawing.Size(40, 23);
+            this.btnSelectLogFile.TabIndex = 2;
+            this.btnSelectLogFile.Text = "...";
+            this.btnSelectLogFile.UseVisualStyleBackColor = true;
+            this.btnSelectLogFile.Click += new System.EventHandler(this.btnSelectLogFile_Click);
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRegex.Location = new System.Drawing.Point(110, 69);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(408, 20);
+            this.txtRegex.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Regex:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(83, 35);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(16, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "...";
+            // 
+            // btnResultsToText
+            // 
+            this.btnResultsToText.Location = new System.Drawing.Point(86, 142);
+            this.btnResultsToText.Name = "btnResultsToText";
+            this.btnResultsToText.Size = new System.Drawing.Size(108, 23);
+            this.btnResultsToText.TabIndex = 7;
+            this.btnResultsToText.Text = "All to text area";
+            this.btnResultsToText.UseVisualStyleBackColor = true;
+            this.btnResultsToText.Click += new System.EventHandler(this.btnResultsToText_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(214, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "All to clipboard";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Append to results:";
+            // 
+            // txtResultFormula
+            // 
+            this.txtResultFormula.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResultFormula.Location = new System.Drawing.Point(110, 98);
+            this.txtResultFormula.Name = "txtResultFormula";
+            this.txtResultFormula.Size = new System.Drawing.Size(408, 20);
+            this.txtResultFormula.TabIndex = 9;
+            this.txtResultFormula.Text = "\\r\\n";
+            // 
+            // txtResults
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.Location = new System.Drawing.Point(86, 171);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResults.Size = new System.Drawing.Size(432, 208);
+            this.txtResults.TabIndex = 11;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 391);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtResultFormula);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnResultsToText);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.btnSelectLogFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLogPath);
+            this.Name = "Form1";
+            this.Text = "Big Log Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLogPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelectLogFile;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnResultsToText;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultFormula;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigLogSearch
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetLabelDelegate(string text);
+
+        string m_Log;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Controls' event handlers
+
+        private void txtLogPath_TextChanged(object sender, EventArgs e)
+        {
+            lblStatus.Text = "...";
+        }
+
+        private void btnResultsToText_Click(object sender, EventArgs e)
+        {
+            txtResults.Text = GetResults();
+        }
+
+        private void btnSelectLogFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                txtLogPath.Text = openFileDialog1.FileName;
+                LoadLog();
+            }
+        }
+
+        #endregion
+
+
+        #region Helper Methods
+
+        private void LoadLog()
+        {
+            new Thread
+            (
+                new ThreadStart
+                (
+                    () =>
+                    {
+                        this.SelLabel("Loading...");
+
+                        DateTime now = DateTime.Now;
+
+                        using (StreamReader reader = new StreamReader(txtLogPath.Text))
+                        {
+                            m_Log = reader.ReadToEnd();
+                        }
+
+                        TimeSpan t = DateTime.Now - now;
+
+                        string timeStr = t.TotalMilliseconds <= 2000
+                                        ? t.TotalMilliseconds.ToString("N0") + " ms"
+                                        : t.TotalSeconds.ToString("N0") + " s";
+
+                        FileInfo fi = new FileInfo(txtLogPath.Text);
+
+                        string sizeStr = fi.Length > 2 * 1048576
+                                        ? (fi.Length / 1048576).ToString() + " MB"
+                                        : fi.Length > 2 * 1024
+                                        ? (fi.Length / 1024).ToString() + " KB"
+                                        : fi.Length + " B";
+
+                        this.SelLabel("Loaded successfully (" + sizeStr + " in " + timeStr + ").");
+                    }
+                )
+            ).Start();
+        }
+
+        private void SelLabel(string text)
+        {
+            if (this.lblStatus.InvokeRequired)
+            {
+                SetLabelDelegate d = new SetLabelDelegate(SelLabel);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lblStatus.Text = text;
+            }
+        }
+
+        private string GetResults()
+        {
+            StringBuilder sbRe = new StringBuilder(txtRegex.Text);
+            sbRe.Replace("\\r", "\r")
+                .Replace("\\n", "\n")
+                .Replace("\\t", "\t");
+
+            //Regex re = new Regex(txtRegex.Text);
+            Regex re = new Regex(sbRe.ToString());
+            var matches = re.Matches(m_Log);
+
+            StringBuilder sbFormula = new StringBuilder(txtResultFormula.Text);
+            sbFormula.Replace("\\r", "\r")
+                .Replace("\\n", "\n")
+                .Replace("\\t", "\t");
+
+            var formula = sbFormula.ToString();
+
+            StringBuilder result = new StringBuilder();
+
+            //MessageBox.Show(matches.Count.ToString());
+
+            //for (var i = 0; i < matches.Count; i++)
+            //for (var i = 0; i < 5; i++)
+            //{
+            //    result.Append(matches[i].Value);
+            //    result.Append(formula);
+            //}
+
+            var match = re.Match(m_Log);
+            while (match.Success)
+            {
+                result.Append(match.Value);
+                result.Append(formula);
+                match = match.NextMatch();
+            }
+
+            return result.ToString();
+        }
+
+        #endregion
+    }
+}
